6c92851c27a12a2c410efb6cb63e9895c3a694c5
barelyThinkingBagOfWater <x.barrelet@gmail.com>
2021-09-17 13:47:17 +0700

    Able to download items as well. Price is properly rounded and only considered in transactions for BNB. Only transactions related to bunnies are now persisted.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from sanic import Sanic, response
 
 import importService
 
-# Consider https://github.com/MagicStack/asyncpg in place of psycopg2
 
 app = Sanic("NFTWS")
 
@@ -18,10 +17,16 @@ async def hello(request):
 
 @app.route('/import')
 async def import_bunnies(request):
-    # Check if db is empty, if not cancel to avoid human errors. Create emptyDb endpoint as well. Later will be incremental
     loop = asyncio.get_event_loop()
     loop.create_task(importService.import_transactions())
     return response.text('Import of transactions started')
+
+
+@app.route('/items')
+async def import_items(request):
+    loop = asyncio.get_event_loop()
+    loop.create_task(importService.import_items())
+    return response.text('Import of items started')
 
 
 if __name__ == '__main__':
